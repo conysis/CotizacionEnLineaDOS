58d1fdf903808ff83741d76a428fa64526bc6af3
alexander.goicoechea <alexander.goicoechea@uned.cr>
2021-08-17 22:58:02 -0600

    Traduccion de mensajes 1

--- CotizacionEnLinea/Server/Startup.cs
+++ CotizacionEnLinea/Server/Startup.cs
@@ -22,8 +22,6 @@ namespace CotizacionEnLinea.Server
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
@@ -45,7 +43,6 @@ namespace CotizacionEnLinea.Server
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -57,10 +54,8 @@ namespace CotizacionEnLinea.Server
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
             app.UseHttpsRedirection();
             app.UseBlazorFrameworkFiles();
             app.UseStaticFiles();
